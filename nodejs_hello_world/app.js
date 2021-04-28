ea5580e0e1757c8e324e188dfe08defdb44d8950
KerstenBreuer <kersten-breuer@outlook.com>
2021-04-28 14:32:24 +0200

    add simple hello world nodejs app

---
+++ nodejs_hello_world/app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const os = require('os');
+const message = "Hello World"
+
+console.log("Server ist starting...");
+
+var handler = function(request, response) {
+    console.log("Received request from " + request.connection.remoteAddress);
+    response.writeHead(200);
+    response.end(message);
+};
+
+var www = http.createServer(handler);
+www.listen(8080);
